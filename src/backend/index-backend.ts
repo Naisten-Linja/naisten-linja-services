fc43637e6270ed8bf8a57da023f5219737bb65b5
nathandao <nathan@guynathan.com>
2020-05-16 23:59:07 +0300

    Init files

---
+++ src/backend/index-backend.ts
@@ -0,0 +1,168 @@
+import crypto, { HexBase64Latin1Encoding } from 'crypto';
+import express, { Request } from 'express';
+import session from 'express-session';
+import url from 'url';
+
+function checkVariables() {
+  ['DISCOURSE_URL', 'BACKEND_URL', 'DISCOURSE_SSO_SECRET', 'COOKIE_SECRET', 'ENVIRONMENT'].forEach((key) => {
+    if (process.env[key] === '' || !process.env[key]) {
+      throw `Missing variable ${key} from your environment`;
+    }
+  });
+}
+
+function getConfig() {
+  checkVariables();
+
+  const backendUrl = process.env.BACKEND_URL!;
+  const { hostname } = url.parse(backendUrl);
+
+  if (hostname === null) {
+    throw `BACKEND_URL has hostname = null!`;
+  }
+  return {
+    discourseUrl: process.env.DISCOURSE_URL!,
+    discourseSsoSecret: process.env.DISCOURSE_SSO_SECRET!,
+    backendUrl: process.env.BACKEND_URL!,
+    cookieSecret: process.env.COOKIE_SECRET!,
+    environment: process.env.ENVIRONMENT!,
+    hostName: hostname,
+  };
+}
+
+// Encode string from a defined encoding to a different form
+const encodeString = (s: string, encodingFrom: BufferEncoding, encodingTo: BufferEncoding) =>
+  Buffer.from(s, encodingFrom).toString(encodingTo);
+
+// Generate a random 16 bytes string and return its base64 encoded value
+const generateNonce = () => crypto.randomBytes(16).toString('base64');
+
+// Generate a HMAC SHA-256 hash using the provided Discourse SSO key
+function hmacSha256(payload: string, encoding?: HexBase64Latin1Encoding) {
+  const { discourseSsoSecret } = getConfig();
+
+  return encoding
+    ? crypto.createHmac('sha256', discourseSsoSecret).update(payload).digest(encoding)
+    : crypto.createHmac('sha256', discourseSsoSecret).update(payload, 'utf8').digest();
+}
+
+// Generate login redirect link and 'nonce' value using steps defined at
+// https://meta.discourse.org/t/using-discourse-as-a-sso-provider/32974
+//
+// - Generate a random 'nonce'. Save it temporarily to compare with returned 'nonce' when user is redirected back from discourse
+// - Create a new query string with nonce and return url (where the Discourse will redirect user after verification). It should look like "nonce=NONCE&return_sso_url=RETURN_URL"
+// - Base64 encode the above query string. Let’s call this 'payload'
+// - Generate a HMAC-SHA256 signature from 'payload' using your sso provider secret as the key, then create a lower case hex string from this. Let’s call this signature as HEX_SIGNATURE
+// - URL encode the above 'payload'. Let’s call this payload as URL_ENCODED_PAYLOAD
+// - Redirect user to the discourse url DISCOURSE_ROOT_URL/session/sso_provider?sso=URL_ENCODED_PAYLOAD&sig=HEX_SIGNATURE
+function createSso(req: Request) {
+  if (!req.session) {
+    return '/';
+  }
+
+  const { backendUrl, discourseUrl } = getConfig();
+  const ssoReturnUrl = `${backendUrl}/auth/sso/verify`;
+  const nonce = generateNonce();
+  const query = `nonce=${nonce}&return_sso_url=${ssoReturnUrl}`;
+  const payload = encodeString(query, 'utf8', 'base64');
+  const sig = hmacSha256(payload, 'hex');
+
+  req.session.nonce = nonce;
+
+  return `${discourseUrl}/session/sso_provider?sso=${encodeURIComponent(payload)}&sig=${sig}`;
+}
+
+function getQueryData(queryString: string) {
+  const queryData = queryString.split('&').reduce<Record<string, string>>(
+    (result, currentVal) => {
+      const [key, val] = currentVal.split('=');
+      return {
+        ...result,
+        [key]: decodeURIComponent(val),
+      };
+    },
+    { nonce: '' },
+  );
+
+  return queryData;
+}
+
+// Validate the sso request after redirected back from Discourse
+//
+// Criterias:
+// - 'sig' and 'sso' exist as get parameters, and are strings
+// - HMAC SHA-256 hash from 'sso' in bytes === 'sig' in byfes
+// - 'nonce' value in the decoded sso query equals 'session.nonce'
+//   ('session.nonce' was set before redirecting user to discourse in '/auth/sso')
+function validateSsoRequest(req: Request) {
+  const { sig, sso } = req.query;
+
+  if (!req.session || !req.session.nonce) {
+    console.log('Session information not found');
+    return false;
+  }
+
+  if (!sig || !sso || typeof sig !== 'string' || typeof sso !== 'string') {
+    console.log('Missing or invalid "sso" or "sig" query parameter');
+    return false;
+  }
+
+  const ssoHmac = hmacSha256(sso) as Buffer;
+  const isValidSignature = Buffer.compare(ssoHmac, Buffer.from(sig, 'hex')) === 0;
+  const ssoStr = encodeString(sso, 'base64', 'utf8');
+  const ssoData = getQueryData(ssoStr);
+  const isValidNonce = req.session.nonce === ssoData.nonce;
+
+  return isValidSignature && isValidNonce;
+}
+
+function createApp(port: number) {
+  const { cookieSecret, hostName, environment } = getConfig();
+
+  const app = express();
+
+  // Required for session cookie to work behind a proxy
+  app.set('trust proxy', 1);
+  app.use(
+    session({
+      secret: cookieSecret,
+      name: 'session',
+      cookie: {
+        secure: environment === 'production',
+        httpOnly: true,
+        domain: hostName,
+        path: '/auth/sso',
+        expires: new Date(Date.now() + 30 * 60 * 1000), // 30 minutes
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => res.send('hello world'));
+
+  app.get('/auth/sso', (req, res) => {
+    if (!req.session) {
+      console.error('Missing Session in request');
+      res.redirect('/');
+      return;
+    }
+    const redirectUrl = createSso(req);
+    res.redirect(redirectUrl);
+  });
+
+  app.get('/auth/sso/verify', (req, res) => {
+    if (!validateSsoRequest(req)) {
+      console.log('Invalid sso return request');
+      res.redirect('/');
+      return;
+    }
+    delete req.session.nonce;
+    const ssoStr = encodeString(`${req.query.sso}`, 'base64', 'utf8');
+    res.send(ssoStr);
+  });
+
+  return app;
+}
+
+const port = 3000;
+const app = createApp(port);
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
