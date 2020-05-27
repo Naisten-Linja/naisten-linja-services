9c9a61f5d1d8d731f8ba5f45fb2b3e951169119c
nathandao <nathan@guynathan.com>
2020-05-27 08:21:54 +0300

    Return json error if jwt is not valid

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -73,6 +73,13 @@ export function createApp() {
       ],
     }),
   );
+  // @ts-ignore
+  app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({ error: 'unauthorized' });
+    }
+    next();
+  });
 
   app.use(
     expressWinston.logger({
