8d792201a79bdd3dea5286ea29c89b043642fa66
nathandao <nathan@guynathan.com>
2020-10-26 21:19:00 +0200

    Add exceptions column to booking type table

---
+++ db/migrations/20201020191130-add-exceptions-columns-to-booking-types.js
@@ -0,0 +1,33 @@
+'use strict';
+const async = require('async');
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  async.series(
+    [db.addColumn.bind(db, 'booking_types', 'exceptions', { type: 'string' })],
+    callback,
+  );
+};
+
+exports.down = function (db) {
+  // Deleting all 'iv' values for existing letters and replies would
+  // make them impossible to decrypt. So this step should not be rolled back.
+  return null;
+};
+
+exports._meta = {
+  version: 1,
+};
