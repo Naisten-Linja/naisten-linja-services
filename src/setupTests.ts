e5f4b161efc8050c2edde255fd7cee498d06dde0
Tiina Kimari <tiina.kimari@futurice.com>
2021-11-06 13:46:59 +0200

    Mock Date

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,12 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+
+const RealDate = Date;
+
+beforeEach(() => {
+  global.Date.now = jest.fn(() => Date.parse('2019-04-22').valueOf());
+});
+
+afterEach(() => {
+  global.Date = RealDate;
+});
