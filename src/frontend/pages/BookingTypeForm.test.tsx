f2998c751407f0bf5ca6d704fb26c2a66e43a808
Tiina Kimari <tiina.kimari@futurice.com>
2021-11-29 18:54:45 +0200

    Show possibility to add additional information to booking type

--- src/frontend/pages/BookingTypeForm.test.tsx
+++ src/frontend/pages/BookingTypeForm.test.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import { BookingTypeForm } from './BookingTypeForm';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Formik } from 'formik';
+import { BookingSlot, BookingTypeDailyRules } from '../../common/constants-common';
+import axios from 'axios';
+
+jest.spyOn(axios, 'post');
 
 describe('BookingTypeForm', () => {
+  const onSubmitMock = jest.fn();
+  const initialValues = {
+    name: '',
+    rules: [0, 1, 2, 3, 4, 5, 6].map(() => ({
+      enabled: true,
+      slots: [] as Array<BookingSlot>,
+    })) as BookingTypeDailyRules,
+    exceptions: [] as Array<string>,
+    additionalInformation: null,
+  };
   beforeEach(() => {
-    render(<BookingTypeForm />);
+    render(
+      <Formik initialValues={initialValues} onSubmit={onSubmitMock}>
+        <BookingTypeForm />
+      </Formik>,
+    );
   });
 
   const addSlotForMonday = () => {
@@ -31,5 +50,37 @@ describe('BookingTypeForm', () => {
     userEvent.click(screen.getByRole('button', { name: 'Add exceptions' }));
 
     await waitFor(() => expect(screen.getByTestId('exceptions-date-picker')).toBeInTheDocument());
+  });
+
+  it('shows a text field for additional information', () => {
+    expect(screen.getByText('Additional information')).toBeInTheDocument();
+  });
+
+  it('when a booking type is added and form is submitted, submits the booking type', async () => {
+    userEvent.type(screen.getByRole('textbox', { name: 'booking type name' }), 'some name');
+    userEvent.type(
+      screen.getByRole('textbox', { name: 'additional information' }),
+      'some additional information',
+    );
+    addSlotForMonday();
+
+    userEvent.click(screen.getByRole('button', { name: 'Save' }));
+
+    await waitFor(() =>
+      expect(axios.post).toHaveBeenCalledWith(
+        '/api/booking-types',
+        expect.objectContaining({
+          name: 'some name',
+          additionalInformation: 'some additional information',
+          rules: expect.arrayContaining([
+            {
+              enabled: true,
+              slots: [{ end: '13:00', seats: 3, start: '12:00' }],
+            },
+          ]),
+        }),
+        expect.anything(),
+      ),
+    );
   });
 });
