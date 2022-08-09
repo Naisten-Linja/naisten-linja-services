23a65e306657cc4d6fb84c82b77cf79b171087cf
andri-yta <andriani.yunita@futurice.com>
2022-08-09 09:10:43 +0300

    Add bg pattern for a fully booked slot

--- src/frontend/BookingCalendar/CalendarColumn.tsx
+++ src/frontend/BookingCalendar/CalendarColumn.tsx
@@ -93,6 +93,15 @@ export const CalendarColumn: React.FC<CalendarColumnProps> = ({
             )?.count || 0;
           const availableSeats = seats - bookedSlotCount;
 
+          // Add diagonal pattern to the background if the seats are fully booked
+          const fullBookingTypeColor = `repeating-linear-gradient(
+            45deg,
+            ${bookingTypeColor},
+            #000 1px,
+            ${bookingTypeColor} 1px,
+            ${bookingTypeColor} 8px
+          );`;
+
           const top = `${(start.diff(currentDate, 'minutes') / 1440) * 100}%`;
           const height = `${(end.diff(start, 'seconds') / 3600) * HOUR_CELL_HEIGHT}rem`;
           // Get overlapping slots
@@ -113,7 +122,7 @@ export const CalendarColumn: React.FC<CalendarColumnProps> = ({
               top={top}
               height={height}
               leftOffset={leftOffset}
-              backgroundColor={end.isBefore(moment()) ? '#8c8c8c' : bookingTypeColor}
+              background={end.isBefore(moment()) ? '#8c8c8c' : (availableSeats > 0) ? bookingTypeColor : fullBookingTypeColor}
               width={slotsInOverlappingZone.length > 1 ? '50%' : '100%'}
               onClick={() =>
                 openBookingForm({
@@ -145,7 +154,7 @@ export const CalendarColumn: React.FC<CalendarColumnProps> = ({
 
 const SlotButton = styled.button<{
   leftOffset: string;
-  backgroundColor: string;
+  background: string;
   top: string;
   height: string;
   width: string;
@@ -162,10 +171,10 @@ const SlotButton = styled.button<{
   padding: 0.5rem;
   position: absolute;
   text-align: left;
-  ${({ backgroundColor, leftOffset, top, height, width }) => css`
+  ${({ background, leftOffset, top, height, width }) => css`
     top: ${top};
     height: ${height};
-    background-color: ${backgroundColor};
+    background: ${background};
     left: ${leftOffset};
     width: ${width};
     &:hover,
@@ -173,7 +182,7 @@ const SlotButton = styled.button<{
     &:focus {
       color: white;
       z-index: 2;
-      background-color: ${backgroundColor};
+      background: ${background};
     }
     &:focus {
       outline: 0.125rem solid #08c;
