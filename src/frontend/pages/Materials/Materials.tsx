4436dcae86a1630446a773650a2ccecf427a524d
andri-yta <andriani.yunita@futurice.com>
2022-08-26 16:04:29 +0300

    Add TL file for materials page

--- src/frontend/pages/Materials/Materials.tsx
+++ src/frontend/pages/Materials/Materials.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
 
+// Use translation
+import { useTranslation } from 'react-i18next';
+import { namespaces } from '../../i18n/i18n.constants';
+
 import { UserRole } from '../../../common/constants-common';
 import { useAuth } from '../../AuthContext';
 import { ContentPage } from '../../ui-components/ContentPage/ContentPage';
 import { EditContentPageForm } from '../../ui-components/ContentPage/EditContentPageForm';
 
 export const Materials: React.FunctionComponent<RouteComponentProps> = () => {
+  const { t } = useTranslation(namespaces.pages.materials);
+
   const [isEditing, setIsEditing] = useState(false);
   const { user } = useAuth();
 
@@ -19,7 +25,7 @@ export const Materials: React.FunctionComponent<RouteComponentProps> = () => {
             className="button margin-top-xs button-primary"
             onClick={() => setIsEditing(true)}
           >
-            Edit page
+            {t('edit_page')}
           </button>
         )}
       </>
