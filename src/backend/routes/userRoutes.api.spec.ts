ea40105710773764f22ad7062dda3a456c9e5dc5
nathandao <nathan.dao@futurice.com>
2022-09-18 09:15:56 +0300

    Add missing env vars in githubci

--- src/backend/routes/userRoutes.api.spec.ts
+++ src/backend/routes/userRoutes.api.spec.ts
@@ -8,81 +8,83 @@ import { IntegrationHelpers } from '../test-utils';
 import { ApiUserData, UserRole } from '../../common/constants-common';
 import { User } from '../models/users';
 
-let app: express.Application;
-let volunteer: User;
-let staff: User;
+describe('userRoutes', () => {
+  let app: express.Application;
+  let volunteer: User;
+  let staff: User;
 
-beforeAll(async () => {
-  app = await IntegrationHelpers.getApp();
+  beforeAll(async () => {
+    app = await IntegrationHelpers.getApp();
 
-  staff = await IntegrationHelpers.createTestUser(
-    {
-      discourseUserId: 1,
-      fullName: 'Staff',
-      email: 'staffUser@naistenlinja.fi',
-    },
-    UserRole.staff,
-  );
-
-  volunteer = await IntegrationHelpers.createTestUser(
-    {
-      discourseUserId: 2,
-      fullName: 'Volunteer User',
-      email: 'volunteerUser@naistenlinja.fi',
-    },
-    UserRole.volunteer,
-  );
-});
-
-afterAll(() => {
-  return IntegrationHelpers.cleanup();
-});
-
-describe('/api/users', () => {
-  it('should not allow public access by default', async () => {
-    const res = await request(app).get('/api/users/').set('Accept', 'application/json');
-    expect(res.statusCode).toEqual(401);
-  });
-
-  it('should not allow volunteers to access user list', async () => {
-    const token = await IntegrationHelpers.getToken(volunteer);
-    const res = await request(app)
-      .get('/api/users/')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ${token?.token}`);
-    expect(res.statusCode).toEqual(403);
-  });
-
-  it('should allow staffs to access user list', async () => {
-    const { token } = await IntegrationHelpers.getToken(staff);
-    const res = await request(app)
-      .get('/api/users/')
-      .set('Accept', 'application/json')
-      .set('Authorization', `Bearer ${token}`);
-
-    const users = (res.body?.data || []) as Array<ApiUserData>;
-    expect(res.statusCode).toEqual(200);
-    expect(users.length).toEqual(2);
-    expect(users).toIncludeAllPartialMembers(
-      [staff, volunteer].map(
-        ({
-          email,
-          uuid,
-          role,
-          fullName,
-          created,
-          userNote,
-          newBookingNotificationDaysThreshold,
-        }) => ({
-          email,
-          uuid,
-          role,
-          fullName,
-          created,
-          userNote,
-          newBookingNotificationDaysThreshold,
-        }),
-      ),
+    staff = await IntegrationHelpers.createTestUser(
+      {
+        discourseUserId: 1,
+        fullName: 'Staff',
+        email: 'staffUser@naistenlinja.fi',
+      },
+      UserRole.staff,
     );
+
+    volunteer = await IntegrationHelpers.createTestUser(
+      {
+        discourseUserId: 2,
+        fullName: 'Volunteer User',
+        email: 'volunteerUser@naistenlinja.fi',
+      },
+      UserRole.volunteer,
+    );
+  });
+
+  afterAll(() => {
+    return IntegrationHelpers.cleanup();
+  });
+
+  describe('GET /api/users', () => {
+    it('should not allow public access by default', async () => {
+      const res = await request(app).get('/api/users/').set('Accept', 'application/json');
+      expect(res.statusCode).toEqual(401);
+    });
+
+    it('should not allow volunteers to access user list', async () => {
+      const token = await IntegrationHelpers.getToken(volunteer);
+      const res = await request(app)
+        .get('/api/users/')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${token?.token}`);
+      expect(res.statusCode).toEqual(403);
+    });
+
+    it('should allow staffs to access user list', async () => {
+      const { token } = await IntegrationHelpers.getToken(staff);
+      const res = await request(app)
+        .get('/api/users/')
+        .set('Accept', 'application/json')
+        .set('Authorization', `Bearer ${token}`);
+
+      const users = (res.body?.data || []) as Array<ApiUserData>;
+      expect(res.statusCode).toEqual(200);
+      expect(users.length).toEqual(2);
+      expect(users).toIncludeAllPartialMembers(
+        [staff, volunteer].map(
+          ({
+            email,
+            uuid,
+            role,
+            fullName,
+            created,
+            userNote,
+            newBookingNotificationDaysThreshold,
+          }) => ({
+            email,
+            uuid,
+            role,
+            fullName,
+            created,
+            userNote,
+            newBookingNotificationDaysThreshold,
+          }),
+        ),
+      );
+    });
   });
 });
